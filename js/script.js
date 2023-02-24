39e9e785a9122a7cba0e97f1779e44a6151bcf37
Matheus Patricio da S e Silva <matheuspatricio.aspx@gmail.com>
2023-02-23 22:10:02 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ const input = document.querySelector('.input_search');
 const fetchPokemon = async (pokemon) =>{
     /*AWAIT: ele espera o fetch carregar para dps passar para o resto do code
     obs:só é possível utilizar o await em funções assíncronas*/
-    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`);
     const data = await APIResponse.json();
     console.log(data);
     return data;
@@ -21,12 +21,11 @@ const renderPokemon = async (pokemon) =>{
     pokemonName.innerHTML = data.name;
     pokemonNumber.innerHTML = data.id;
     pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+
+    input.value ='';
 }
 
 form.addEventListener('submit', (event) => {
-
     event.preventDefault();
-    console.log('input.value')
-
     renderPokemon(input.value);
 });
